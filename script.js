904f2e1090425234b90784a35266d956a141c13c
neshamervin <n01729514@humber.ca>
2025-01-18 19:16:17 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// Declare Variables:
+const studentName = 'Nesha';
+let studentAge = 24;
+let isEnrolled = true;
+
+console.log(studentName, studentAge, isEnrolled);
+
+// Demonstrate Data Types:
+const courseCode = 'CPAN-113';
+console.log(courseCode);
+
+let numOfStudents = 58;
+console.log(numOfStudents);
+
+let isJavaScriptCool = true;
+console.log(isJavaScriptCool);
+
+const listOfDataTypes = ['String', 'Number', 'Boolean', 'Array', 'Object'];
+console.log(listOfDataTypes);
+
+const person = {
+  name: 'Nesha',
+  age: 24,
+  hobbies: ['working out', 'trying new foods', 'reading'],
+};
+console.log(person);
+
+// Use Arithmetic Operators:
+const additionOperator = 5 + 4;
+console.log(`5 + 4 = ${additionOperator}`);
+
+const subtractionOperator = 10 - 8;
+console.log(`10 - 8 = ${subtractionOperator}`);
+
+const multiplicationOperator = 27 * 6;
+console.log(`27 * 6 = ${multiplicationOperator}`);
+
+const divisionOperator = 36 / 6;
+console.log(`36 / 6 = ${divisionOperator}`);
+
+// Use Comparison Operators:
+const greaterThanOperator = 20 > 5;
+console.log(`20 > 5: ${greaterThanOperator}`);
+
+const lessThanOperator = 18 < 100;
+console.log(`18 < 100: ${lessThanOperator}`);
+
+const equalToOperator = '20' === 20;
+console.log(`'20' === 20: ${equalToOperator}`);
+
+const notEqualToOperator = 'banana' !== 'apple';
+console.log(`'banana' !== 'apple': ${notEqualToOperator}`);
+
+// Use Logical Operators:
+const andOperator = 20 > 10 && '100' == 100;
+console.log(`20 > 10 && '100' == 100: ${andOperator}`);
+
+const orOperator = true || false;
+console.log(`true || false: ${orOperator}`);
+
+const notOperator = 1000 != 100;
+console.log(`1000 != 100: ${notOperator}`);
